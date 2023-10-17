4ae00a9a4a1677ce5f18cbbe1a0410336f5b801c
Nami <havenothorn@gmail.com>
2023-10-17 16:26:23 +0900

    add: Chapter 8. 클래스 공부 내용 업데이트

--- 08/index.js
+++ 08/index.js
@@ -1,6 +1,7 @@
 // Chapter 8. 클래스
 // 8.1 클래스 메서드
 var _a;
+var _Base_truePrivate;
 class Greeter {
     greet(name) {
         console.log(`${name}, do your stuff!`);
@@ -40,8 +41,8 @@ class WithPropertyParameters {
         this.takesParameters = (input) => (input ? "Yes" : "No");
     }
 }
-const instance = new WithPropertyParameters();
-instance.takesParameters(true); // Ok
+const instance0 = new WithPropertyParameters();
+instance0.takesParameters(true); // Ok
 // instance.takesParameters(123); // Error
 // 초기화 검사
 class WithValue {
@@ -102,3 +103,221 @@ class Teacher {
 let teacher;
 teacher = new Teacher();
 // teacher = "Wahoo"; // Error
+// 타입으로서의 클래스
+class SchoolBus {
+    getAbilities() {
+        return ["magic", "shapeshifting"];
+    }
+}
+function withSchoolBus(bus) {
+    console.log(bus.getAbilities());
+}
+withSchoolBus(new SchoolBus()); // Ok
+// Ok
+withSchoolBus({
+    getAbilities: () => ["transmogrification"],
+});
+class Student {
+    constructor(name) {
+        this.name = name;
+    }
+    study(hours) {
+        for (let i = 0; i < hours; i += 1) {
+            console.log("...studying...");
+        }
+    }
+}
+class ReportCard {
+    constructor(grades) {
+        this.grades = grades;
+    }
+    report() {
+        return this.grades.join(",");
+    }
+}
+// class AsNumber implements AgeIsANumber, AgeIsNotANumber {
+//   age = 0; // Error
+// }
+// class NotAsNumber implements AgeIsANumber, AgeIsNotANumber {
+//   age() {
+//     return ""; // Error
+//   }
+// }
+// 클래스 확장
+class Teacher0 {
+    teach() {
+        console.log("The surest test of discipline is its absence.");
+    }
+}
+class StudentTeacher extends Teacher0 {
+    learn() {
+        console.log("I cannot afford the luxury of a closed mind.");
+    }
+}
+const teacher0 = new StudentTeacher();
+teacher0.teach(); // Ok (기본 클래스에 정의됨)
+teacher0.learn(); // Ok (하위 클래스에 정의됨)
+// teacher0.other(); // Error
+// 할당 가능성 확장
+class Lesson {
+    constructor(subject) {
+        this.subject = subject;
+    }
+}
+class OnlineLesson extends Lesson {
+    constructor(subject, url) {
+        super(subject);
+        this.url = url;
+    }
+}
+let lesson;
+lesson = new Lesson("coding"); // Ok
+lesson = new OnlineLesson("coding", "oreilly.com"); // Ok
+let online;
+online = new OnlineLesson("coding", "oreilly.com"); // Ok
+// online = new Lesson("coding"); // Error
+class PastGrades {
+    constructor() {
+        this.grades = [];
+    }
+}
+class LabeledPastGrades extends PastGrades {
+}
+let subClass;
+subClass = new LabeledPastGrades();
+subClass = new PastGrades();
+// 재정의된 생성자
+class GradeAnnouncer {
+    constructor(grade) {
+        this.message = grade >= 65 ? "Maybe next time..." : "You Pass!";
+    }
+}
+class PassingAnnouncer extends GradeAnnouncer {
+    constructor() {
+        super(100);
+    }
+}
+// class FailingAnnouncer extends GradeAnnouncer {
+//   constructor() {
+//   }  // Error
+// }
+class GradesTally {
+    constructor() {
+        this.grades = [];
+    }
+    addGrades(...grades) {
+        this.grades.push(...grades);
+        return this.grades.length;
+    }
+}
+class ContinuedGradesTally extends GradesTally {
+    constructor(previousGrades) {
+        // this.grades = [...previousGrades]; // Error
+        super();
+        console.log("Starting with length", this.grades.length); // Ok
+    }
+}
+// 재정의된 메서드
+class GradeCounter {
+    countGrades(grades, letter) {
+        return grades.filter((grade) => grade === letter).length;
+    }
+}
+class FailureCounter extends GradeCounter {
+    countGrades(grades) {
+        return super.countGrades(grades, "F");
+    }
+}
+// class AnyFailureChecker extends GradeCounter {
+//   countGrades(grades: string[]) {
+//     return super.countGrades(grades, "F") !== 0; // Error
+//   }
+// }
+const counter = new FailureCounter();
+// 예상한 타입: number
+// 실제 타입: boolean
+const count = counter.countGrades(["A", "C", "F"], "F");
+// 재정의된 속성
+class Assignment {
+}
+class GradeAssignment extends Assignment {
+    constructor(grade) {
+        super();
+        this.grade = grade;
+    }
+}
+class NumericGrade {
+    constructor() {
+        this.value = 0;
+    }
+}
+class VagueGrade extends NumericGrade {
+}
+const instance = new VagueGrade();
+// 예상한 타입: number
+// 실제 타입: number | string
+instance.value;
+// 추상 클래스
+class School {
+    constructor(name) {
+        this.name = name;
+    }
+}
+class PreSchool extends School {
+    getStudentTypes() {
+        return ["preschooler"];
+    }
+}
+// class Absence extends School {}
+let school;
+school = new PreSchool("Sunnyside Daycare"); // Ok
+// school = new School("somewhere else"); // Error
+// 멤버 접근성
+class Base {
+    constructor() {
+        this.isPublicImplicit = 0;
+        this.isPublicExplicit = 1;
+        this.isProtected = 2;
+        this.isPrivate = 3;
+        _Base_truePrivate.set(this, 4);
+    }
+}
+_Base_truePrivate = new WeakMap();
+class Subclass extends Base {
+    examples() {
+        this.isPublicImplicit; // Ok
+        this.isPublicExplicit; // Ok
+        this.isProtected; // Ok
+        // this.isPrivate; // Error
+        // this.#truePrivate; // Error
+    }
+}
+new Subclass().isPublicImplicit; // Ok
+new Subclass().isPublicExplicit; // Ok
+// new Subclass().isProtected; // Error
+// new Subclass().isPrivate; // Error
+class TwoKeywords {
+    constructor() {
+        this.name = "Anne Sullivan"; // Ok
+    }
+    log() {
+        console.log(this.name); // Ok
+    }
+}
+const two = new TwoKeywords();
+// two.name = "Savitribal phule"; // Error
+// 정적 필드 제한자
+class Question {
+    guess(getAnswer) {
+        const answer = getAnswer(Question.prompt);
+        // Ok
+        if (answer === Question.answer) {
+            console.log("You got it");
+        }
+        else {
+            console.log("Try again...");
+        }
+    }
+}
+Question.prompt = `What's an ogre's favorite programming language?`;
+// Question.answer;
--- 08/index.ts
+++ 08/index.ts
@@ -55,9 +55,9 @@ class WithPropertyParameters {
   takesParameters = (input: boolean) => (input ? "Yes" : "No");
 }
 
-const instance = new WithPropertyParameters();
+const instance0 = new WithPropertyParameters();
 
-instance.takesParameters(true); // Ok
+instance0.takesParameters(true); // Ok
 // instance.takesParameters(123); // Error
 
 // 초기화 검사
@@ -144,3 +144,337 @@ class Teacher {
 let teacher: Teacher;
 teacher = new Teacher();
 // teacher = "Wahoo"; // Error
+
+// 타입으로서의 클래스
+class SchoolBus {
+  getAbilities() {
+    return ["magic", "shapeshifting"];
+  }
+}
+
+function withSchoolBus(bus: SchoolBus) {
+  console.log(bus.getAbilities());
+}
+
+withSchoolBus(new SchoolBus()); // Ok
+
+// Ok
+withSchoolBus({
+  getAbilities: () => ["transmogrification"],
+});
+
+// withSchoolBus({
+//   getAbilities: () => 123, // Error
+// });
+
+// 클래스와 인터페이스
+interface Learner {
+  name: string;
+  study(hours: number): void;
+}
+
+class Student implements Learner {
+  name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  study(hours: number) {
+    for (let i = 0; i < hours; i += 1) {
+      console.log("...studying...");
+    }
+  }
+}
+
+// class Slacker implements Learner {
+//     // Error
+// }
+
+// class Student0 implements Learner {
+//   name;
+//     // Error
+//   study(hours) {
+//     // Error
+//   }
+// }
+
+// 다중 인터페이스 구현
+
+interface Graded {
+  grades: number[];
+}
+
+interface Reporter {
+  report: () => string;
+}
+
+class ReportCard implements Graded, Reporter {
+  grades: number[];
+
+  constructor(grades: number[]) {
+    this.grades = grades;
+  }
+
+  report() {
+    return this.grades.join(",");
+  }
+}
+
+// class Empty implements Graded, Reporter {} // Error
+
+interface AgeIsANumber {
+  age: number;
+}
+
+interface AgeIsNotANumber {
+  age: () => string;
+}
+
+// class AsNumber implements AgeIsANumber, AgeIsNotANumber {
+//   age = 0; // Error
+// }
+
+// class NotAsNumber implements AgeIsANumber, AgeIsNotANumber {
+//   age() {
+//     return ""; // Error
+//   }
+// }
+
+// 클래스 확장
+class Teacher0 {
+  teach() {
+    console.log("The surest test of discipline is its absence.");
+  }
+}
+
+class StudentTeacher extends Teacher0 {
+  learn() {
+    console.log("I cannot afford the luxury of a closed mind.");
+  }
+}
+
+const teacher0 = new StudentTeacher();
+teacher0.teach(); // Ok (기본 클래스에 정의됨)
+teacher0.learn(); // Ok (하위 클래스에 정의됨)
+
+// teacher0.other(); // Error
+
+// 할당 가능성 확장
+class Lesson {
+  subject: string;
+
+  constructor(subject: string) {
+    this.subject = subject;
+  }
+}
+
+class OnlineLesson extends Lesson {
+  url: string;
+
+  constructor(subject: string, url: string) {
+    super(subject);
+    this.url = url;
+  }
+}
+
+let lesson: Lesson;
+lesson = new Lesson("coding"); // Ok
+lesson = new OnlineLesson("coding", "oreilly.com"); // Ok
+
+let online: OnlineLesson;
+
+online = new OnlineLesson("coding", "oreilly.com"); // Ok
+// online = new Lesson("coding"); // Error
+
+class PastGrades {
+  grades: number[] = [];
+}
+
+class LabeledPastGrades extends PastGrades {
+  label?: string;
+}
+
+let subClass: LabeledPastGrades;
+
+subClass = new LabeledPastGrades();
+subClass = new PastGrades();
+
+// 재정의된 생성자
+class GradeAnnouncer {
+  message: string;
+
+  constructor(grade: number) {
+    this.message = grade >= 65 ? "Maybe next time..." : "You Pass!";
+  }
+}
+
+class PassingAnnouncer extends GradeAnnouncer {
+  constructor() {
+    super(100);
+  }
+}
+
+// class FailingAnnouncer extends GradeAnnouncer {
+//   constructor() {
+//   }  // Error
+// }
+
+class GradesTally {
+  grades: number[] = [];
+
+  addGrades(...grades: number[]) {
+    this.grades.push(...grades);
+    return this.grades.length;
+  }
+}
+
+class ContinuedGradesTally extends GradesTally {
+  constructor(previousGrades: number[]) {
+    // this.grades = [...previousGrades]; // Error
+
+    super();
+
+    console.log("Starting with length", this.grades.length); // Ok
+  }
+}
+
+// 재정의된 메서드
+class GradeCounter {
+  countGrades(grades: string[], letter: string) {
+    return grades.filter((grade) => grade === letter).length;
+  }
+}
+
+class FailureCounter extends GradeCounter {
+  countGrades(grades: string[]) {
+    return super.countGrades(grades, "F");
+  }
+}
+
+// class AnyFailureChecker extends GradeCounter {
+//   countGrades(grades: string[]) {
+//     return super.countGrades(grades, "F") !== 0; // Error
+//   }
+// }
+
+const counter: GradeCounter = new FailureCounter();
+
+// 예상한 타입: number
+// 실제 타입: boolean
+const count = counter.countGrades(["A", "C", "F"], "F");
+
+// 재정의된 속성
+class Assignment {
+  grade?: number;
+}
+
+class GradeAssignment extends Assignment {
+  grade: number;
+
+  constructor(grade: number) {
+    super();
+    this.grade = grade;
+  }
+}
+
+class NumericGrade {
+  value = 0;
+}
+
+class VagueGrade extends NumericGrade {
+  //   value = Math.random() > 0.5 ? 1 : "..."; // Error
+}
+
+const instance: NumericGrade = new VagueGrade();
+
+// 예상한 타입: number
+// 실제 타입: number | string
+instance.value;
+
+// 추상 클래스
+abstract class School {
+  readonly name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  abstract getStudentTypes(): string[];
+}
+
+class PreSchool extends School {
+  getStudentTypes() {
+    return ["preschooler"];
+  }
+}
+
+// class Absence extends School {}
+
+let school: School;
+
+school = new PreSchool("Sunnyside Daycare"); // Ok
+
+// school = new School("somewhere else"); // Error
+
+// 멤버 접근성
+
+class Base {
+  isPublicImplicit = 0;
+  public isPublicExplicit = 1;
+  protected isProtected = 2;
+  private isPrivate = 3;
+  #truePrivate = 4;
+}
+
+class Subclass extends Base {
+  examples() {
+    this.isPublicImplicit; // Ok
+    this.isPublicExplicit; // Ok
+    this.isProtected; // Ok
+
+    // this.isPrivate; // Error
+    // this.#truePrivate; // Error
+  }
+}
+
+new Subclass().isPublicImplicit; // Ok
+new Subclass().isPublicExplicit; // Ok
+
+// new Subclass().isProtected; // Error
+// new Subclass().isPrivate; // Error
+
+class TwoKeywords {
+  private readonly name: string;
+
+  constructor() {
+    this.name = "Anne Sullivan"; // Ok
+  }
+
+  log() {
+    console.log(this.name); // Ok
+  }
+}
+
+const two = new TwoKeywords();
+
+// two.name = "Savitribal phule"; // Error
+
+// 정적 필드 제한자
+class Question {
+  protected static readonly answer: "bash";
+  protected static readonly prompt = `What's an ogre's favorite programming language?`;
+
+  guess(getAnswer: (prompt: string) => string) {
+    const answer = getAnswer(Question.prompt);
+
+    // Ok
+    if (answer === Question.answer) {
+      console.log("You got it");
+    } else {
+      console.log("Try again...");
+    }
+  }
+}
+
+// Question.answer;
